d6feef2343e562a24681e2e2df625af3b98667c9
Vitalii Hoshuliak <goshulyakvitalik@gmail.com>
2026-07-27 13:44:14 +0100

    13/44

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
 import React from 'react';
 
 export class App extends React.Component {
+  state = {
+    pressedKey: '',
+  };
 
-    state = {
-      pressedKey: ''
-    };
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({
+      pressedKey: event.key,
+    });
+  };
 
-    handleKeyUp = (event: KeyboardEvent) => {
-  this.setState({
-    pressedKey: event.key,
-  });
-};
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
 
-    componentDidMount() {
-  document.addEventListener('keyup', this.handleKeyUp);
-}
-
-    componentWillUnmount() {
+  componentWillUnmount() {
     document.removeEventListener('keyup', this.handleKeyUp);
-    }
+  }
 
   render() {
-
     return (
-
-  <div className="App">
-    <p className="App__message">The last pressed key is [{this.state.pressedKey === ''
-  ? 'Nothing was pressed yet'
-  : this.state.pressedKey}]</p>
-  </div>
+      <div className="App">
+        <p className="App__message">
+          The last pressed key is
+          {this.state.pressedKey === ''
+            ? 'Nothing was pressed yet'
+            : this.state.pressedKey}
+          
+        </p>
+      </div>
     );
   }
 }
